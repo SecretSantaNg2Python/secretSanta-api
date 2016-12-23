8130cef4f536816525ffd850fe0e16618832f3d5
nathan-j-brenner <nathan.j.brenner@gmail.com>
2016-12-23 11:46:35 -0800

    set config settings

---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'C}{,8%:8KkjgYrrm+G&)qt-dz_G*{f,Tgr75'
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
