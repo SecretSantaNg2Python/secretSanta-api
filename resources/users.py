4feeb408110430d7ca41f43e1557078a55aa75ab
nathan-j-brenner <nathan.j.brenner@gmail.com>
2016-12-23 15:47:17 -0800

    added endpoint to get all users

--- resources/users.py
+++ resources/users.py
@@ -45,6 +45,10 @@ class UserList(Resource):
         )
         super().__init__()
 
+    def get(self):
+        users = [marshal(user, user_fields) for user in models.User.select()]
+        return {'users': users}
+
     def post(self):
         args = self.reqparse.parse_args()
         if args['password'] == args['verify_password']:
